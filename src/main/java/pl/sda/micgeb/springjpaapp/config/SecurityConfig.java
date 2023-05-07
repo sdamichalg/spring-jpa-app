21e3d25e626babdcee901bd41954fa2113ccbee3
Michal Gebski <gebski.michal@gmail.com>
2023-05-07 11:55:34 +0200

    10. komentarz do zadania z rolami

---
+++ src/main/java/pl/sda/micgeb/springjpaapp/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package pl.sda.micgeb.springjpaapp.config;
+
+public class SecurityConfig {
+
+//    1. Stworzenie bean SecurityFilterChain
+//    2. swtorzenie beana do encoder oraz użytkowników(usatwienie username, password, roles)
+//    3. dodać odpowiednie role na api (np. @RolesAllowed)
+//    4. dodanie .permitAll (opcjonalnie)
+}
