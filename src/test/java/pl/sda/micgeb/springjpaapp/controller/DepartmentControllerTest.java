50e551031f4422ef24eb10f88f870696b2e8c628
Michal Gebski <gebski.michal@gmail.com>
2023-05-07 14:20:29 +0200

    12. mvc test

---
+++ src/test/java/pl/sda/micgeb/springjpaapp/controller/DepartmentControllerTest.java
@@ -0,0 +1,52 @@
+package pl.sda.micgeb.springjpaapp.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@WebAppConfiguration
+class DepartmentControllerTest {
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+//    {
+//        "name": "IT",
+//            "address": {
+//                 "country": "Poland",
+//                "city": "Warsaw",
+//                "street": "Nowy Świat",
+//                "zipCode": "00-001"
+//    }
+//    }
+
+    @Test
+    void getDepartmentById() throws Exception {
+        //given
+        //when and //then
+        mockMvc.perform(get("/department/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("IT"))
+        ;
+    }
+}
