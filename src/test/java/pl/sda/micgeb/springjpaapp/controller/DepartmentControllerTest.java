d0f159e5702cf7649b80cf9bf90df96ebdf58d1f
Michal Gebski <gebski.michal@gmail.com>
2023-05-07 14:55:31 +0200

    mvc with post test

--- src/test/java/pl/sda/micgeb/springjpaapp/controller/DepartmentControllerTest.java
+++ src/test/java/pl/sda/micgeb/springjpaapp/controller/DepartmentControllerTest.java
@@ -1,5 +1,7 @@
 package pl.sda.micgeb.springjpaapp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import pl.sda.micgeb.springjpaapp.model.Address;
+import pl.sda.micgeb.springjpaapp.model.dtos.DepartmentDto;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -47,13 +51,12 @@ class DepartmentControllerTest {
 
     @Test
     void getDepartmentsByCity() throws Exception {
-//        department2.setAddress(new Address("Poland", "Krakow", "Rynek Główny", "31-042"));
 
         mockMvc.perform(get("/department/v1?name=Sales"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].name").value("Sales"))
                 .andExpect(jsonPath("$[0].address.country").value("Poland"))
                 .andExpect(jsonPath("$[0].address.city").value("Krakow"))
@@ -61,6 +64,22 @@ class DepartmentControllerTest {
                 .andExpect(jsonPath("$[0].address.zipCode").value("31-042"))
 
         ;
+    }
 
+    @Test
+    void addNewDepartment() throws Exception {
+        //given
+        DepartmentDto dto = new DepartmentDto();
+        dto.setName("new_department");
+        dto.setAddress(new Address());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonBody = objectMapper.writeValueAsString(dto);//zamieniamy obiekt na json
+
+        //when         //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/department/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
